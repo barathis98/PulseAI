1bae2fb1eb807f663e525218adfaf7414c6aa261
Barathi Sridhar <sridhar.ba@northeastern.edu>
2024-12-02 18:56:15 -0500

    added cors

--- langchain-env/src/api.py
+++ langchain-env/src/api.py
@@ -2,10 +2,24 @@ import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from src.qa_system import ask_question
+from fastapi.middleware.cors import CORSMiddleware
 
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow these origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 class QuestionRequest(BaseModel):
     question: str
