e7e55b57bae4e76fb95b5b1fb8425505deae48b2
Barathi Sridhar <sridhar.ba@northeastern.edu>
2024-12-02 21:40:35 -0500

    added my domain

--- langchain-env/src/api.py
+++ langchain-env/src/api.py
@@ -9,7 +9,8 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://pulse-ui-six.vercel.app/"
 
 ]
 
